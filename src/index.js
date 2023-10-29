0693b9cc6eb03f64a7a73d75fbbb121f90c434d5
Adarsh Rangare <akrangare@gmail.com>
2023-10-29 15:53:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 //Write your code here
 
-
 class API {
-  
-  url;
-  method;
   #secure;
 
   constructor(url, method = 'GET') {
     this.url = url;
     this.method = method;
-   // this.#secure = url.startsWith('https');
+    this.#secure = url.startsWith('https');
   }
 
   isSecure() {
-    return this.url.startsWith("https");
+    return this.#secure;
   }
 
   updateURL(newURL) {
     this.url = newURL;
-   // this.#secure = newURL.startsWith('https');
+    this.#secure = newURL.startsWith('https');
   }
 }
-
-
 
 //Do not remove this code
 module.exports = { API }
